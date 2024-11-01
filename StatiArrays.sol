645178e0fb5127bb58bf0222fe37d14d62fb19b7
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-11-01 21:32:52 +0900

    Update StatiArrays.sol

--- StatiArrays.sol
+++ StatiArrays.sol
@@ -14,7 +14,7 @@ contract StaticArrays {
         storageBoolArray = [true, true, true, false, false];   // 이미 선언된 storage 배열 채워 넣기.
 
         uint[2] memory memoryUintArray;           // 함수 안에서 선언된 배열의 저장 장소는 memory 이다. 
-        memoryUintArray = [uint(1), 2];               // 정적 memory 배열 채워 넣기.
+        memoryUintArray = [uint(1), 2];           // 정적 memory 배열 채워 넣기.
         bool[2] memory memoryBoolArray = [true, false];     // 정적 memory 배열 초기화 가능.            
 	      
         memoryUintArray[0] = 0;       // OK.
